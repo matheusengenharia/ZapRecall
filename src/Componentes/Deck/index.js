bd58cf26cdf6f47c6e5388ccdaaea3142405213d
Matheus Vasconcelos <matheus.vasconcelos@engenharia.ufjf.br>
2023-10-15 17:16:15 -0300

    Criando segunda tela

--- src/Componentes/Deck/index.js
+++ src/Componentes/Deck/index.js
@@ -1,6 +1,24 @@
+
+import LogoPequeno from "./../../assets/img/logo-pequeno.png";
+import "./style.css";
+import data from "./../../index.js";
+
 function Deck(){
     return(
-        <h1>teste deck</h1>
+        <div className="Deck">
+            <header>
+                <img src={LogoPequeno} alt="Logo ZaReCall"/>
+                <h1>ZaReCall</h1>
+                <main>
+                    <p>Flashacards</p>
+                </main>
+                <footer>
+                    <p>0/4 Concluídos</p>
+                </footer>
+            </header>
+
+        </div>
+
     )
 }
 
--- src/Componentes/Inicio/index.js
+++ src/Componentes/Inicio/index.js
@@ -1,12 +1,13 @@
 import { useState} from "react";
 
-import Logo from "./../../assets/img/logo.png";
+import Logo from "./../../assets/img/logo.png"; // Importa a imagem como módulo
+import "./style.css"; // Importa o CSS como módulo
 
 function Inicio(){
     const [visivel, setVisivel] = useState(true);
     if(visivel){
     return(
-        <div>
+        <div className="Inicio">
         <img src={Logo} alt="Logo Zap Recall"/>
         <h1>Zap-ReCall</h1>
         <button onClick={()=> setVisivel(false)}>Iniciar!</button>
